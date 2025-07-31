cb8169a1d0b81372967c178b302101ef555d1b66
costa-rica <nrodrig1@gmail.com>
2025-07-30 18:27:10 -0700

    modif: AdminSettings - toggling visibility status

--- screens/AdminSettings.js
+++ screens/AdminSettings.js
@@ -15,6 +15,9 @@ import {
 import TemplateViewWithTopChildrenSmall from "./subcomponents/TemplateViewWithTopChildrenSmall";
 import { useSelector, useDispatch } from "react-redux";
 import { useState, useEffect } from "react";
+import BtnVisibilityDown from "../assets/images/buttons/btnVisibilityDown.svg";
+import BtnVisibilityUp from "../assets/images/buttons/btnVisibilityUp.svg";
+
 import { updateTeamsArray } from "../reducers/user";
 // import { updateReviewReducerSelectedVideoObject } from "../reducers/review";
 import {
@@ -32,6 +35,7 @@ export default function AdminSettings({ navigation }) {
   const userReducer = useSelector((state) => state.user);
   const uploadReducer = useSelector((state) => state.upload);
   const teamReducer = useSelector((state) => state.team);
+  const [showVisibilityOptions, setShowVisibilityOptions] = useState(false);
 
   const topChildren = (
     <Text>
@@ -40,9 +44,43 @@ export default function AdminSettings({ navigation }) {
     </Text>
   );
 
-  useEffect(() => {
-    console.log(JSON.stringify(teamReducer.teamsArray));
-  }, []);
+  useEffect(() => {}, [teamReducer.teamsArray]);
+
+  const updateTeamVisibility = async (visibility) => {
+    console.log(`---> update Team Visibility status: ${visibility}`);
+
+    const bodyObj = {
+      teamId: teamReducer.teamsArray.filter((team) => team.selected)[0].id,
+      visibility: visibility,
+    };
+    const response = await fetch(
+      `${process.env.EXPO_PUBLIC_API_URL}/teams/update-visibility`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userReducer.token}`,
+        },
+        body: JSON.stringify(bodyObj),
+      }
+    );
+    let resJson = null;
+    const contentType = response.headers.get("Content-Type");
+    if (contentType?.includes("application/json")) {
+      resJson = await response.json();
+    }
+
+    if (response.ok && resJson) {
+      let tempArray = [...teamReducer.teamsArray];
+      tempArray.filter((team) => team.selected)[0].visibility = visibility;
+      dispatch(updateTeamsArray(tempArray));
+    } else {
+      const errorMessage =
+        resJson?.error ||
+        `There was a server error (and no resJson): ${response.status}`;
+      alert(errorMessage);
+    }
+  };
 
   return (
     <TemplateViewWithTopChildrenSmall
@@ -81,12 +119,65 @@ export default function AdminSettings({ navigation }) {
             </View>
             <View style={styles.vwTeamVisibility}>
               <Text style={styles.txtTeamVisibilityTitle}>Visibility</Text>
-              <Text style={styles.txtTeamVisibilityValue}>
-                {
-                  teamReducer.teamsArray.filter((team) => team.selected)[0]
-                    .visibility
-                }
-              </Text>
+              <TouchableOpacity
+                style={[
+                  styles.touchableOpacityVisibilityCapsule,
+                  styles.vwDropdownOptionCapsule,
+                ]}
+                onPress={() => setShowVisibilityOptions(!showVisibilityOptions)}
+              >
+                <Text style={styles.txtVisibilityCapsule}>
+                  {
+                    teamReducer.teamsArray.filter((team) => team.selected)[0]
+                      .visibility
+                  }
+                </Text>
+                {showVisibilityOptions ? (
+                  <BtnVisibilityUp />
+                ) : (
+                  <BtnVisibilityDown />
+                )}
+              </TouchableOpacity>
+              {showVisibilityOptions && (
+                <View style={styles.vwVisibilityDropdown}>
+                  {[
+                    { type: "Public", value: "Anyone can join" },
+                    {
+                      type: "On invitation",
+                      value: "Only people with link can join",
+                    },
+                    { type: "Private", value: "No one can join" },
+                  ]
+                    .filter(
+                      (option) =>
+                        option.type !==
+                        teamReducer.teamsArray.filter(
+                          (team) => team.selected
+                        )[0].visibility
+                    )
+                    .map((option) => (
+                      <TouchableOpacity
+                        key={option.type}
+                        style={styles.touchableOpacityDropdownOption}
+                        onPress={() => {
+                          updateTeamVisibility(option.type); // Trigger your function
+                          setShowVisibilityOptions(false); // Close dropdown
+                        }}
+                      >
+                        {/* <View style={styles.vwDropdownOption}> */}
+                        <View style={styles.vwDropdownOptionCapsule}>
+                          <Text style={styles.txtDropdownOption}>
+                            {option.type}
+                          </Text>
+                        </View>
+                        <Text style={styles.txtDropdownOptionValue}>
+                          {option.value}
+                        </Text>
+                        {/* </View> */}
+                      </TouchableOpacity>
+                    ))}
+                </View>
+              )}
             </View>
           </View>
         </View>
@@ -112,7 +203,7 @@ const styles = StyleSheet.create({
   // Top
   // ------------
   containerTop: {
-    flex: 1,
+    // flex: 1,
     width: "100%",
     borderColor: "gray",
     borderWidth: 1,
@@ -138,7 +229,7 @@ const styles = StyleSheet.create({
   vwTeamDescription: {
     borderBottomColor: "gray",
     borderBottomWidth: 1,
-    width: "100%",
+    // width: "100%",
     marginBottom: 10,
   },
   txtTeamDescriptionTitle: {
@@ -147,6 +238,63 @@ const styles = StyleSheet.create({
   },
   txtTeamDescriptionValue: {
     fontSize: 16,
+  },
+  vwTeamVisibility: {
+    // borderBottomColor: "gray",
+    // borderBottomWidth: 1,
+    width: "50%",
+    marginBottom: 10,
+  },
+  touchableOpacityVisibilityCapsule: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 12,
+    // paddingVertical: 6,
+    // borderWidth: 1,
+    // borderColor: "gray",
+    // borderRadius: 20,
+    // backgroundColor: "#f5f5f5",
+    marginTop: 5,
+  },
+  txtVisibilityCapsule: {
+    fontSize: 14,
+  },
+  arrow: {
+    marginLeft: 8,
+  },
+  vwVisibilityDropdown: {
+    position: "absolute",
+    top: 50, // adjust for your layout
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 8,
+    width: Dimensions.get("window").width * 0.8,
+    zIndex: 10,
+    elevation: 5,
+  },
+  touchableOpacityDropdownOption: {
+    padding: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+  },
+  vwDropdownOptionCapsule: {
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 20,
+    backgroundColor: "#f5f5f5",
+    width: Dimensions.get("window").width * 0.3,
+    paddingLeft: 5,
+    paddingVertical: 3,
+  },
+  txtDropdownOption: {
+    fontSize: 14,
+  },
+  txtDropdownOptionValue: {
+    fontSize: 12,
+    color: "gray",
   },
   // ------------
   // Bottom
