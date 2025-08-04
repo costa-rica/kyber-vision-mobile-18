2d949b1f765622937d3fd997967e0d3c325e029e
costa-rica <nrodrig1@gmail.com>
2025-08-04 15:50:07 -0700

    modif to version number - splash screen

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -108,7 +108,7 @@ export default function SplashScreen({ navigation }) {
             continue without login
           </ButtonKvStd>
           <Text style={{ position: "absolute", bottom: 20, right: 30 }}>
-            Version 0.17.7
+            Version 0.17.8
           </Text>
         </View>
       </View>
