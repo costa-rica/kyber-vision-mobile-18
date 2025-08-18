f301f371f21f65d11455d2e4f364c00e11365d4b
Nick Rodriguez <nrodrig1@gmail.com>
2025-08-17 17:25:31 -0700

    modif: ScriptingLiveLandscape.js styling left side - needs work but on good path

--- screens/subcomponents/ScriptingLiveLandscape.js
+++ screens/subcomponents/ScriptingLiveLandscape.js
@@ -8,6 +8,9 @@ import * as ScreenOrientation from "expo-screen-orientation";
 import Lightning from "../../assets/images/lightning.svg";
 import Volleyball from "../../assets/images/volleyball.svg";
 import { useSelector } from "react-redux";
+import ButtonKvImage from "./buttons/ButtonKvImage";
+import BtnService from "../../assets/images/buttons/btnService.svg";
+import BtnReception from "../../assets/images/buttons/btnReception.svg";
 
 export default function ScriptingLiveLandscape(props) {
   const teamReducer = useSelector((state) => state.team);
@@ -39,6 +42,26 @@ export default function ScriptingLiveLandscape(props) {
     console.log("<--- [ScriptingLiveLandscape] in handleBackPress");
   };
 
+  // -----------------
+  //  Styles
+  // -----------------
+
+  const stylesBtnBottom = {
+    width: Dimensions.get("window").width * 0.1,
+    height: Dimensions.get("window").width * 0.1,
+    // zIndex: 2,
+    // backgroundColor: "white",
+    marginLeft: -Dimensions.get("window").width * 0.1,
+    marginTop: Dimensions.get("window").width * 0.01,
+  };
+  const stylesBtnTop = {
+    width: Dimensions.get("window").width * 0.1,
+    height: Dimensions.get("window").width * 0.1,
+    // zIndex: 2,
+    marginRight: -Dimensions.get("window").width * 0.1,
+    marginTop: -Dimensions.get("window").width * 0.1,
+  };
+
   return (
     <TemplateViewWithTopChildrenSmallLandscape
       navigation={props.navigation}
@@ -47,7 +70,41 @@ export default function ScriptingLiveLandscape(props) {
       onBackPress={handleBackPress}
     >
       <View style={styles.container}>
-        <View style={[stylesContainer, styles.containerLeft]}></View>
+        <View style={[stylesContainer, styles.containerLeft]}>
+          <View style={styles.vwContainerLeftTop}>
+            <View style={[styles.vwContainerLeftTopLayer, { zIndex: 0 }]}>
+              <View style={styles.vwGroupButtonsCircle} />
+            </View>
+            <View style={[styles.vwContainerLeftTopLayer, { zIndex: 1 }]}>
+              <View style={styles.vwGroupButtonsCircleWrapper}>
+                <View style={styles.vwGroupButtonsDiagonalLine} />
+              </View>
+            </View>
+            <View style={[styles.vwContainerLeftTopLayer, { zIndex: 2 }]}>
+              {/* <Text> Foreground</Text> */}
+
+              <ButtonKvImage
+                onPress={() => {
+                  console.log("pressed reception");
+                }}
+                style={styles.btnRallyGroupTop}
+              >
+                <BtnReception style={stylesBtnTop} />
+              </ButtonKvImage>
+            </View>
+            <View style={[styles.vwContainerLeftTopLayer, { zIndex: 3 }]}>
+              <ButtonKvImage
+                onPress={() => {
+                  console.log("pressed service");
+                }}
+                style={styles.btnRallyGroupBottom}
+              >
+                <BtnService style={stylesBtnBottom} />
+              </ButtonKvImage>
+            </View>
+          </View>
+          <View style={styles.vwContainerLeftBottom}></View>
+        </View>
         <GestureHandlerRootView
           style={[stylesContainer, styles.containerMiddle]}
         >
@@ -65,6 +122,10 @@ export default function ScriptingLiveLandscape(props) {
     </TemplateViewWithTopChildrenSmallLandscape>
   );
 }
+
+const CIRCLE_SIZE = 100;
+const DIAGONAL_LEN = Math.ceil(CIRCLE_SIZE * Math.SQRT2); // ~141
+const LINE_THICKNESS = 8;
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
@@ -90,12 +151,101 @@ const styles = StyleSheet.create({
     color: "white",
   },
 
+  // -----
+  // LEFT
+  // -----
   containerLeft: {
-    backgroundColor: "blue",
+    // backgroundColor: "blue",
+    flex: 1,
   },
+  vwContainerLeftTop: {
+    flex: 1,
+    // height: "50%",
+    // backgroundColor: "red",
+    borderWidth: 1,
+    borderColor: "gray",
+    borderStyle: "dashed",
+  },
+
+  // LAYER
+  vwContainerLeftTopLayer: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0, // fills parent
+    alignItems: "center", // center the text horizontally
+    justifyContent: "center", // center the text vertically
+  },
+
+  // BACKGROUND LAYER
+  vwGroupButtonsCircle: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: "#806181",
+    opacity: 0.12,
+  },
+
+  // MIDGROUND LAYER
+  vwGroupButtonsCircleWrapper: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    // backgroundColor: "#806181",
+    // opacity: 0.12,
+    overflow: "hidden",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  vwGroupButtonsDiagonalLine: {
+    width: DIAGONAL_LEN, // long enough to cross the circle’s corners
+    height: LINE_THICKNESS, // line thickness
+    borderRadius: LINE_THICKNESS / 2, // rounded ends (optional)
+    backgroundColor: "#806181",
+    opacity: 0.8,
+    transform: [{ rotate: "-45deg" }], // top-right → bottom-left
+  },
+
+  // FOREGROUND LAYER
+
+  btnRallyGroupBottom: {
+    paddingHorizontal: 0,
+    // borderColor: "gray",
+    // borderWidth: 1,
+    // borderStyle: "dashed",
+    paddingTop: 50,
+  },
+  // vwGroupButtonsCircle: {
+  //   height: 50,
+  //   width: 50,
+  //   borderRadius: 25,
+  //   // borderRadius: (Dimensions.get("window").width * 0.2) / 2,
+  //   // backgroundColor: "gray",
+  //   // width: Dimensions.get("window").width * 0.2,
+  //   // height: Dimensions.get("window").width * 0.2,
+  //   // top: Dimensions.get("window").width * 0.05,
+  //   // left:
+  //   //   (Dimensions.get("window").width * 0.4) / 2 -
+  //   //   (Dimensions.get("window").width * 0.2) / 2,
+  //   // position: "absolute",
+  //   backgroundColor: "#806181",
+  //   opacity: 0.5,
+  // },
+  vwContainerLeftBottom: {
+    height: 100,
+    backgroundColor: "green",
+  },
+  // -----
+  // MIDDLE
+  // -----
   containerMiddle: {
     backgroundColor: "yellow",
   },
+
+  // -----
+  // RIGHT
+  // -----
   containerRight: {
     backgroundColor: "green",
   },
